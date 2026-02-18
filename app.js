99dd1b60f85f39b261c5a519ce0199fee97136f7
Abhi Talati <forfoxy027@gmail.com>
2026-02-18 23:35:39 +0530

    trail 2

---
+++ app.js
@@ -0,0 +1,71 @@
+console.log("~ Created by Yantram Devs. in 2025");
+console.log("Yantram Devs. is running by Abhi Talati");
+
+const table = document.getElementById("table");
+const save = document.getElementById("save");
+document.getElementById("add-weight").addEventListener('submit' , (e)=>{
+    e.preventDefault();
+})
+
+const rawdata = localStorage.getItem("Data");
+//console.log(rawdata)
+
+if(rawdata != null){
+    const realdata = JSON.parse(rawdata);
+    console.log(realdata);
+
+    for(let i = 0 ; i < realdata.length ; i++){
+        makeCards(realdata[i].date , realdata[i].weight);
+    }
+    
+    function makeCards(realdate , realweight){
+        const tr = document.createElement('tr');
+        const date = document.createElement('td');
+        const weight = document.createElement('td');
+
+        date.textContent = realdate;
+        weight.textContent = realweight;
+        tr.appendChild(date);
+        tr.appendChild(weight);
+
+        table.appendChild(tr);
+
+    }
+
+    save.onclick = function(){
+    const dateEntered = document.getElementById("date").value;
+    const weightEnterd = document.getElementById("weight").value;
+    (dateEntered == null || weightEnterd == null || weightEnterd == ""
+    ) ? alert("Enter Fields properly!!") : addingData();
+
+    }
+    
+}
+else{
+    save.onclick = function (){
+    const dateEntered = document.getElementById("date").value;
+    const weightEnterd = document.getElementById("weight").value;
+    (dateEntered == null || weightEnterd == null || weightEnterd == ""
+    ) ? alert("Enter Fields properly!!") : addingData();
+
+    function addingData(){
+        const data = [];
+        const push = {w_id : 1 , date : dateEntered , weight : weightEnterd};
+        data.push(push);
+        const localdata = JSON.stringify(data);
+        //console.log(localdata);
+        localStorage.setItem("Data" , localdata);
+
+        const tr = document.createElement('tr');
+        const date = document.createElement('td');
+        const weight = document.createElement('td');
+
+        date.textContent = dateEntered;
+        weight.textContent = weightEnterd;
+        tr.appendChild(date);
+        tr.appendChild(weight);
+
+        table.appendChild(tr);
+    }
+}
+}
--- script.js
+++ script.js
@@ -2,12 +2,27 @@ console.log("~ Created by Yantram Devs. in 2025");
 console.log("Yantram Devs. is running by Abhi Talati");
 
 const table = document.getElementById("table");
+const save = document.getElementById("save");
+document.getElementById("add-weight").addEventListener('submit' , (e)=>{
+    e.preventDefault();
+})
+
+save.onclick = () => {
+    addData();
+}
 
 function addData(){
+    
     const dateEntered = document.getElementById("date").value;
     const weightEnterd = document.getElementById("weight").value;
-    
-    console.log(dateEntered , weightEnterd);
+
+    if(dateEntered == null || dateEntered == "" || weightEnterd == null || weightEnterd == NaN || weightEnterd == ""){
+        if(weightEnterd <= 0 )alert("Enter correct Weight & Date!");
+        else alert("Enter the given fields!");
+    }
+
+    else{    
+
     const tr = document.createElement('tr');
     const date = document.createElement('td');
     const weight = document.createElement('td');
@@ -19,6 +34,32 @@ function addData(){
     tr.appendChild(weight);
 
     table.appendChild(tr);
-
+    checkStrg(dateEntered , weightEnterd);
 }
 
+function checkStrg(date , weight){
+    if(localStorage.getItem("Data")){
+        const rawdata = localStorage.getItem("Data");
+        const realdata = JSON.parse(rawdata);
+        console.log(realdata);
+
+        let lastid = realdata.length;
+        console.log(lastid);
+
+        for(let i = 0 ; i < realdata.length ; i++){
+            console.log(realdata[i].w_id);
+        }
+    } 
+    else{
+        const data = [];
+        const insert = {w_id : 1 , date : date , weight : weight}
+
+        data.push(insert);
+        console.log(data)
+        console.log(insert);
+        const localdata = JSON.stringify(data);
+        console.log(localdata);
+        localStorage.setItem("Data" , localdata);
+    }
+}
+}
