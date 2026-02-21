a27e433488c18fa0bb11bc1a5f4c7d7aa0ffe664
Abhi Talati <forfoxy027@gmail.com>
2026-02-21 20:15:56 +0530

    version 1.2.0

--- app.js
+++ app.js
@@ -85,15 +85,3 @@ else{
     }
 }
 }
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./serviceWorker.js")
-      .then((registration) => {
-        console.log("Service Worker registered:", registration);
-      })
-      .catch((error) => {
-        console.log("Service Worker registration failed:", error);
-      });
-  });
-}
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,44 +1,20 @@
-const CACHE_NAME = "my-pwa-cache-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./icon.svg"
-];
-
-// Install event
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log("Opened cache");
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Activate event
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
+self.addEventListener("install" , (e)=>{
+  e.waitUntil(
+    caches.open('wc-cache').then((cache)=>{
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js'
+      ]);
     })
   );
 });
 
-// Fetch event
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+self.addEventListener("fetch" , (e)=>{
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
